5e7f153146ec00e152e873afe980b8813c0f63f4
sasafrita <sabrinaealvarez@gmail.com>
2018-07-10 23:59:16 -0300

    error

--- TPs/TP5/controllers/dataController.js
+++ TPs/TP5/controllers/dataController.js
@@ -36,6 +36,22 @@ self.movies= [
     {id: "m15", movietitle: "Where the Wild Things Are", release: "2009", director: "Spike Jonze", poster: "wtwta.jpg", synopsis: "Max imagines running away from his mom and sailing to a far-off land where large talking beasts – Ira, Carol, Douglas, the Bull, Judith and Alexander – crown him as their king, play rumpus, build forts and discover secret hideaways.", genre: "Science Fiction", rate: "G", watched: false},
 
     {id: "m16", movietitle: "What We Do in the Shadows", release: "2014", director: "Taika Waititi", poster: "wwdits.jpg", synopsis: "Vampire housemates try to cope with the complexities of modern life and show a newly turned hipster some of the perks of being undead.", genre: "Comedy", rate: "PG-13", watched: false},
+
+    {id: "m17", movietitle: "Dirty Computer", release: "2018", director: "Andrew Donoho", poster: "dirtyc.jpg", synopsis: "The story of a young woman named Jane 57821, who is living in a totalitarian near-future society where citizens are referred to as ‘computers.’ ‘Dirty Computer’ explores humanity and what truly happens to life, liberty and the pursuit of happiness when mind and machines merge, and when the government chooses fear over freedom.", genre: "Science Fiction", rate: "R", watched: false},
+
+    {id: "m18", movietitle: "Prisoners", release: "2013", director: "Denis Villeneuve", poster: "prisoners.jpg", synopsis: "When Keller Dover’s daughter and her friend go missing, he takes matters into his own hands as the police pursue multiple leads and the pressure mounts. But just how far will this desperate father go to protect his family?", genre: "Crime", rate: "R", watched: false},
+
+    {id: "m19", movietitle: "Little Miss Sunshine", release: "2006", director: "Valerie Faris", poster: "lms.jpg", synopsis: "A family loaded with quirky, colorful characters piles into an old van and road trips to California for little Olive to compete in a beauty pageant.", genre: "Drama", rate: "PG-13", watched: false},
+
+    {id: "m20", movietitle: "Ruby Sparks", release: "2012", director: "Valerie Faris", poster: "ruby.jpg", synopsis: "Calvin is a young novelist who achieved phenomenal success early in his career but is now struggling with his writing – as well as his romantic life. Finally, he makes a breakthrough and creates a character named Ruby who inspires him. When Calvin finds Ruby, in the flesh, sitting on his couch about a week later, he is completely flabbergasted that his words have turned into a living, breathing person.", genre: "Romance", rate: "PG-13", watched: false},
+
+    {id: "m21", movietitle: "Beetlejuice", release: "1988", director: "Tim Burton", poster: "beetlejuice.jpg", synopsis: "Thanks to an untimely demise via drowning, a young couple end up as poltergeists in their New England farmhouse, where they fail to meet the challenge of scaring away the insufferable new owners, who want to make drastic changes. In desperation, the undead newlyweds turn to an expert frightmeister, but he’s got a diabolical agenda of his own.", genre: "Fantasy", rate: "PG-13", watched: false},
+
+    {id: "m22", movietitle: "Pride and Prejudice", release: "2005", director: "Joe Wright", poster: "mr-darcy-tkm.jpg", synopsis: "Pride & Prejudice is a humorous story of love and life among English gentility during the Georgian era. Mr. Bennet is an English gentleman living in Hertfordshire with his overbearing wife and five daughters. If Mr. Bennet dies their house will be inherited by a distant cousin whom they have never met, so the family’s future happiness and security is dependent on the daughters making good marriages.", genre: "Romance", rate: "G", watched: false},
+
+    {id: "m23", movietitle: "Megamind", release: "2010", director: "Tom McGrath", poster: "megamind.jpg", synopsis: "Bumbling supervillain Megamind finally defeats his nemesis, the superhero Metro Man. But without a hero, he loses all purpose and must find new meaning to his life.", genre: "Animation", rate: "G", watched: false},
+
+    {id: "m24", movietitle: "Hot Rod", release: "2007", director: "Akiva Schaffer", poster: "hot-rod.jpg", synopsis: "For Rod Kimball, performing stunts is a way of life, even though he is rather accident-prone. Poor Rod cannot even get any respect from his stepfather, Frank, who beats him up in weekly sparring matches. When Frank falls ill, Rod devises his most outrageous stunt yet to raise money for Frank’s operation – and then Rod will kick Frank’s butt.", genre: "Comedy", rate: "PG-13", watched: false},
 ];
 
 module.exports= self;
